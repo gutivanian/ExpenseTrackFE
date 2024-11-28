102157b7afe7aa23f10d12b8ec9eeeaddd9e9b54
gutivanian <gutivanaliefs@gmail.com>
2024-11-28 23:27:47 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',  // Menyambungkan frontend ke backend
+      '/api': 'http://expensetrackbe-production.up.railway.app',  // Menyambungkan frontend ke backend
     },
   },
 });
